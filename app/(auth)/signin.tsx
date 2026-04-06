58707ca6b67b29034720ea7a402ed0a6a63b7a59
rebel600 <shyampareek232@gmail.com>
2026-04-06 15:07:44 +0530

    feat: project-setup, nativewind, rounting and tab-navigations

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -1,13 +1,14 @@
 import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SingIn = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">>
       <Text>Singin</Text>
       <Link href="/signup">Create an account</Link>
-    </View>
+    </SafeAreaView>
   );
 };
 
--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,13 +1,15 @@
 import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignUp = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">>
       <Text>SignUp</Text>
       <Link href="/(auth)/signin">Already have an account</Link>
-    </View>
+      <Link href="/">Go to Home</Link>
+    </SafeAreaView>
   );
 };
 
