0d0dca89083c3f8daf20e81b85472c58449e6305
rebel600 <shyampareek232@gmail.com>
2026-04-06 20:20:58 +0530

    refactor utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,8 +5,6 @@ export const formatCurrency = (value: number, currency = "USD"): string => {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency,
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
     }).format(value);
   } catch {
     return value.toFixed(2);
